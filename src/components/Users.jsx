9d73c2d340f40c84ab06871c05617c727ac5bdd6
Jesus Ramos <jeenrade@gmail.com>
2022-05-14 19:49:12 -0400

    second commit

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,23 +1,27 @@
 import React, { useState } from "react";
+import {BsSearch} from 'react-icons/bs'
 
 import { useFilter } from "../hook/useUser";
-import CardComponet from  "./CardComponent.js";
+import CardComponet from "./CardComponent.js";
 
 export const Users = () => {
   const [getUsersFilteredBy, deleteUsers] = useFilter();
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
 
-  const handleChange = (value) => setText(value.target.value)
-
+  const handleChange = (value) => setText(value.target.value);
 
   return (
-  <div>
-    <div>
-      <input type="text"  onChange={handleChange} />
+    <div className="page">
+      <h1>Perfiles de usuario</h1>
+      <div className="finderContainer">
+        <input type="text" className="search" onChange={handleChange}/>
+        <label htmlFor="" className="search-icon"> <BsSearch/> </label>
+      </div>
+      <div className="container">
+        {getUsersFilteredBy(text).map((user) => (
+          <CardComponet onDelete={deleteUsers} key={user.email} user={user} />
+        ))}
+      </div>
     </div>
-    <div className="container">
-      { getUsersFilteredBy(text).map(user => <CardComponet onDelete={ deleteUsers } key={user.email} user={user} />) }
-    </div>
-  </div>
   );
 };
